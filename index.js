fd7b7cdf07f4e3ab9f7cb0ddd2db185934b04fda
Daniel Shikoff <danielshikoff@Daniels-MacBook-Pro.local>
2018-02-27 16:41:46 -0800

    completed checkpoint

---
+++ index.js
@@ -0,0 +1,34 @@
+const pixelContainer = document.querySelector('.pixel-col')
+for(let i = 1; i <= 1012; i++){
+  let div = document.createElement('div')
+  div.classList.add('pixel', 'grey')
+  pixelContainer.appendChild(div)
+}
+
+const pixel = document.querySelectorAll('.pixel')
+let changeClass = function (event){
+  if(pixelContainer.classList.value.includes('pixel-col')){
+    event.target.classList.value = ''
+    event.target.classList.add(colorSelector, 'pixel')
+  }
+}
+
+pixelContainer.addEventListener('click', changeClass)
+
+let colorSelector;
+ document.querySelector('.red').addEventListener('click', function(){
+   colorSelector = 'red'
+   console.log('the color is now red')
+ })
+ document.querySelector('.green').addEventListener('click', function(){
+   colorSelector = 'green'
+    console.log('the color is now green')
+ })
+ document.querySelector('.blue').addEventListener('click', function(){
+   colorSelector = 'blue'
+    console.log('the color is now blue')
+ })
+ document.querySelector('.grey1').addEventListener('click', function(){
+   colorSelector = 'grey'
+    console.log('the color is now grey')
+ })
